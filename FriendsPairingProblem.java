05a341e84bd351a5bfce80923d572a940f5a1652
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-07 11:06:56 +0530

    Create FriendsPairingProblem.java

---
+++ FriendsPairingProblem.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countFriendsPairings(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        long mod = 1000000007;
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + (i - 1) * dp[i - 2]) % mod;
+        }
+        return (int) dp[n];
+    }
+}
