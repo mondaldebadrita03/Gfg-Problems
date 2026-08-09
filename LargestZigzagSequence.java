6e23e1e6727385bb55a1361b6f50d647ee8cc28c
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-09 22:03:02 +0530

    Create LargestZigzagSequence.java

---
+++ LargestZigzagSequence.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int zigzagSequence(int[][] mat) {
+        int n = mat.length;
+        int[] dp = new int[n];
+        for (int j = 0; j < n; j++) {
+            dp[j] = mat[0][j];
+        }
+        for (int i = 1; i < n; i++) {
+            int max1 = Integer.MIN_VALUE;
+            int max2 = Integer.MIN_VALUE;
+            int maxIndex = -1;
+            for (int j = 0; j < n; j++) {
+                if (dp[j] > max1) {
+                    max2 = max1;
+                    max1 = dp[j];
+                    maxIndex = j;
+                } else if (dp[j] > max2) {
+                    max2 = dp[j];
+                }
+            }
+            int[] newDp = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (j != maxIndex) {
+                    newDp[j] = mat[i][j] + max1;
+                } else {
+                    newDp[j] = mat[i][j] + max2;
+                }
+            }
+            dp = newDp;
+        }
+        int ans = Integer.MIN_VALUE;
+        for (int x : dp) {
+            ans = Math.max(ans, x);
+        }
+        return ans;
+    }
+}
